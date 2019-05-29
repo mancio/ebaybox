ffb37afa9dc1e558346e54c7353aabcca4c57ec8
Andrea Mancini <mancioman2@gmail.com>
2019-05-29 22:39:48 +0200

    new

--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./center.component.css']
 })
 export class CenterComponent implements OnInit {
+  kg: string;
+  img: string;
+  dim: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.kg = '10';
+    this.img = 'https://cdn.shopify.com/s/files/1/2604/1662/products/mysterybox_1024x1024@2x.png?v=1514434361';
+    this.dim = 150;
   }
 
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  image: string;
+  dim: number;
+  image2: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.image = 'https://www.harveynorman.com.au/blog/assets/Shopping-Online-Part-4-1000x747.jpg';
+    this.image2 = 'http://clipart-library.com/image_gallery/170138.jpg';
+    this.dim = 150;
   }
 
 }
