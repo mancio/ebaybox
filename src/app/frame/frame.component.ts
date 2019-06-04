dc6c26e9cd2a48b8e068219ea31a3a7aad3275bf
Andrea Mancini <mancioman2@gmail.com>
2019-06-04 12:52:54 +0200

    new

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {log} from 'util';
 
 
 @Component({
@@ -11,15 +12,23 @@ export class FrameComponent implements OnInit {
   basicurl: string;
   pics: number;
 
+
   constructor() { }
 
   ngOnInit() {
     this.basicurl = 'https://mancioboxblog.altervista.org/ebay/box/mini/';
     this.pics = 27;
-  }
+}
 
   counter(i: number) {
     return new Array(i);
+  }
+
+  radomize(min: number, max: number){
+    let ran;
+    ran = Math.floor(Math.random() * (max - min + 1)) + min;
+    log(ran);
+    return ran;
   }
 
 }
